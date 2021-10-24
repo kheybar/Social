12b7182d71bebe0c03a62ab4fed86e8bdf31016e
mahdi zarepour <mahdizarepour15@gmail.com>
2021-10-24 23:44:45 +0330

    Extended Profile to User Models. Use Signal too.

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin # ظاهر ادمین که در قسمت ادمین مشاهده میکنیم
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Register your models here.
+
+# اینلاین یک قرارداد بین برنامه نویسان است
+class ProfileInline(admin.StackedInline): # برای تعیین نحوه نمایش در ادمین جنگو از این کلاس ارث بری میکنه
+    model = Profile
+    can_delete = False # کسی نتونه از طریق پنل ادمین حذفش کنه
+
+
+class ExtendedProfileAdmin(UserAdmin): # برای برقراری ارتباط با با پنل ادمین از این کلاس ارث بری میکنه
+    inlines = (ProfileInline, )
+
+
+admin.site.unregister(User)
+admin.site.register(User, ExtendedProfileAdmin)

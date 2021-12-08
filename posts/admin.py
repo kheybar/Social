891b2be7dfa8251bd537bde87f1c68180154e285
mahdi zarepour <mahdizarepour15@gmail.com>
2021-12-08 11:55:58 +0330

    Add New Feature To Admin Panel(App Post).

--- posts/admin.py
+++ posts/admin.py
@@ -1,8 +1,35 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils import html
 from .models import Post, Comment, Vote
 
 
 
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        '__str__',
+        'slug',
+        'created',
+        )
+    readonly_fields = ('show_url',)
+
+    def show_url(self, instance):
+        url = reverse(
+            'posts:post_detail',
+            kwargs={
+                'year': instance.created.year,
+                'month': instance.created.month,
+                'day': instance.created.day,
+                'slug': instance.slug,
+            }
+        )
+        response = html.format_html("""<a href="{0}"> {0} </a>""", url)
+        return response
+
+
+
+@admin.register(Comment)
 class CommentManager(admin.ModelAdmin):
     list_display = (
         'user',
@@ -12,11 +39,4 @@ class CommentManager(admin.ModelAdmin):
 
 
 
-admin.site.register(Post)
-
-admin.site.register(
-    Comment,
-    CommentManager,
-    )
-    
 admin.site.register(Vote)
